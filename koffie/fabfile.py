13838781f156483938ebe35f8bca9c6c2a57cb28
Thijs de Zoute <thijsdezoete@gmail.com>
2012-11-22 10:44:09 +0100

    deploy updates

--- koffie/fabfile.py
+++ koffie/fabfile.py
@@ -1,4 +1,7 @@
+from __future__ import with_statement
 from fabric.api import local
+from fabric.api import local, settings, abort, run, cd
+from fabric.contrib.console import confirm
 
 def test():
     local('./manage.py test article')
@@ -13,3 +16,14 @@ def prepare_deploy():
     test()
     commit()
     push()
+
+def deploy():
+    code_dir = '/opt/domains/kopjekoffie.eu/src/kopjekoffie'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            code_dir = '/opt/domains/kopjekoffie.eu/src/'
+            run("git clone git@github.com:thijsdezoete/kopjekoffie.git %s" % code_dir)
+            code_dir = '/opt/domains/kopjekoffie.eu/src/kopjekoffie'
+    with cd(code_dir):
+         run("git pull")
+         run("touch app.wsgi")

969ef7faf43e8e962cb3d8ce683131dc66959fe4
Thijs de Zoete <thijsdezoete@gmail.com>
2013-07-29 21:46:49 +0200

    Open up teh website

--- koffie/article/views.py
+++ koffie/article/views.py
@@ -9,13 +9,21 @@ from django.conf import settings
 
 def index(request):
     something = Article.objects.all().order_by('-votes_up', '-added_date')
-    return render_to_response('article/index.html', {'articles':something})
+    ads_settings = {
+        'client': settings.ADS_CLIENT,
+        'slot': settings.ADS_SLOT
+    }
+    context = {}
+    context.update({'ads': ads_settings, 'articles': something})
 
-@login_required
+    return render_to_response('article/index.html', context)
+
+
 def browse_tag(request, tagname):
     _tag = get_object_or_404(Tag, name=tagname)
     articles_by_tag = Article.objects.filter(tags=_tag).order_by('-votes_up', '-added_date')
-    return render_to_response('article/index.html', {'articles':articles_by_tag})
+    return render_to_response('article/index.html', {'articles': articles_by_tag})
+
 
 @login_required
 def add_article(request):
